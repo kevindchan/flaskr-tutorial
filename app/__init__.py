48f5e8b0db0a035472117fb33e3321edf2bc98f1
Kevin Chan <kevin.chan@alayacare.com>
2018-04-28 15:26:17 -0400

    Fixes Schema spacing, establishes db connection.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -29,4 +30,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello World!'
 
+    db.init_app(app)
+
     return app
+
--- app/db.py
+++ app/db.py
@@ -7,7 +7,7 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite.connect(
+        g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
@@ -21,3 +21,23 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized DB')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
